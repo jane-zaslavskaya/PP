b6bdcee496b0cf98af7d7cc09f2c34b36c0fd7d4
syler <slavik_br@live.ru>
2015-04-05 22:52:54 +0300

    added base repository interface

---
+++ PP/PP.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using PP.Core.Data.Base;
+
+namespace PP.DAL.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        void Dispose();
+
+        /// <summary>
+        /// The save.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        void Save(T entity);
+
+        int AddOrUpdate(T entity);
+
+        /// <summary>
+        /// The remove.
+        /// </summary>
+        /// <param name="entity">
+        /// The entity.
+        /// </param>
+        void Remove(T entity);
+
+        /// <summary>
+        /// The search for.
+        /// </summary>
+        /// <param name="predicate">
+        /// The predicate.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IQueryable"/>.
+        /// </returns>
+        IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate);
+
+        /// <summary>
+        /// The get all.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IQueryable"/>.
+        /// </returns>
+        IQueryable<T> GetAll();
+
+        /// <summary>
+        /// The get by id.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="T"/>.
+        /// </returns>
+        T GetById(long id);
+
+        /// <summary>
+        /// Saves the changes.
+        /// </summary>
+        void SaveChanges();
+    }
+}

63478b66823891e8ad5aced568aaa271178ea1e4
syler <slavik_br@live.ru>
2015-04-08 00:05:56 +0300

    oAuth configuration

---
+++ PP/PP.API/Providers/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.OAuth;
+using PP.BL.Interfaces;
+
+namespace PP.API.Providers
+{
+    public class SimpleAuthorizationServerProvider
+        : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-All-Origin", new[] { "*" });
+
+            var authService = (IAuthBl)System.Web.Mvc.DependencyResolver.Current.GetService(typeof(IAuthBl));
+            var user = await authService.FindUser(context.UserName, context.Password);
+            if (user == null)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect");
+                return;
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+        }
+    }
+}
--- PP/PP.API/Startup.cs
+++ PP/PP.API/Startup.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
+using System;
+using System.Web.Http;
+using PP.API.Providers;
 
 [assembly: OwinStartup(typeof(PP.API.Startup))]
 
@@ -11,7 +13,31 @@ namespace PP.API
     {
         public void Configuration(IAppBuilder app)
         {
+            var config = new HttpConfiguration();
+
+            ConfigureOpenAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+
             App_Start.AutoMapper.Map();
+        }
+
+        private void ConfigureOpenAuth(IAppBuilder app)
+        {
+            var oAuthAuthorixationServer =
+                new OAuthAuthorizationServerOptions
+                {
+                    AllowInsecureHttp = true,
+                    TokenEndpointPath = new PathString("/token"),
+                    AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                    Provider = new SimpleAuthorizationServerProvider()
+                };
+
+            // Token generation
+            app.UseOAuthAuthorizationServer(oAuthAuthorixationServer);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
         }
     }
 }
